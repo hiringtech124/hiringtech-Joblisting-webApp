1dfc910ddfabae9eb93b5ca5c41277edcd4141d1
Faizan Ansari <hussainfaizan1379@gmail.com>
2024-09-12 12:41:00 +0530

    bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // if using Next.js app directory
+    './pages/**/*.{js,ts,jsx,tsx}', // Ensure you're watching all JS/TS files in pages folder
+    './components/**/*.{js,ts,jsx,tsx}', // Same for components folder
+    './app/**/*.{js,ts,jsx,tsx}', // If you're using the app directory (Next.js 13+)
   ],
   theme: {
     extend: {},
